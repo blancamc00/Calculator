9bc124e4e524334e496008918813052d989ce547
Blanca Manu <blanca.manu@softwire.com>
2022-07-04 17:31:21 +0100

    Add a simple multiplier

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the calculator!");
+            Console.WriteLine("Enter first number");
+            string firstNumberString = Console.ReadLine();
+            double firstNumber = Double.Parse(firstNumberString);
+            
+            Console.WriteLine("Enter second number");
+            string secondNumberString = Console.ReadLine();
+            double secondNumber = Double.Parse(secondNumberString);
+
+            double sol = firstNumber * secondNumber;
+            Console.WriteLine("Solution: " + sol);
         }
     }
 }
